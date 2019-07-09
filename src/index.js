08603d1c572e0db44fe2bd4121e4ccce2b3d23f6
Scott H <zenwattage@gmail.com>
2019-07-09 12:36:16 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/*put stuff in*/}
+            </button>
+
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                
+            </div>
+        )
+    }
+}
